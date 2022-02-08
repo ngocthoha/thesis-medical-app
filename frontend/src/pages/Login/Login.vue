9f8ba193a91d63f58558272c3c4072741ab91ab4
Thanh Nguyen <thanhbkcse@gmail.com>
2022-02-09 00:24:24 +0700

    Update Login.vue

--- frontend/src/pages/Login/Login.vue
+++ frontend/src/pages/Login/Login.vue
@@ -3,10 +3,14 @@
         <div class="md-layout">
             <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-60">
                 <div class="login-section col">
-                    <strong>Đăng nhập</strong>
-                    <label for="">Tên đăng nhập</label>
-                    
-                    <label for="">Mật khẩu</label>
+                    <h3>Đăng nhập</h3>
+                    <div class="login-section-body">
+                        <label for="">Tên đăng nhập</label>
+                        <input type="text" name="" id="username">
+                        <label for="">Mật khẩu</label>
+                        <input type="password" name="" id="password">
+                        <md-button class="login-button">Đăng nhập</md-button>
+                    </div>
                 </div>
                 <div class="login-img col">
                     <img src="@/assets/img/login-image.png" alt="doctor" class="login-img">
@@ -19,7 +23,10 @@
 <style lang="scss" scoped>
     .md-layout-item{
         background: white;
-        margin:auto;
+        margin-top: 100px;
+        margin-left: auto;
+        margin-right: auto;
+        padding: 0;
     }
     .content .login-img{
         width: 50%px;
@@ -29,5 +36,18 @@
     .col{
         width: 50%;
         float: left;
+    }
+    .login-section label{
+        display: block;
+    }
+    .login-button{
+        display: block;
+    }
+    .login-section h3{
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    .login-section-body{
+        margin-left: 30%;
     }
 </style>
