56546d49356198f53095c08ce1151b27dad8c4d6
thanhbkcse <thanhbkcse@gmail.com>
2022-03-23 19:20:06 +0700

    update

--- frontend/src/pages/Appointment/MakeAppointmentByDate.vue
+++ frontend/src/pages/Appointment/MakeAppointmentByDate.vue
@@ -10,16 +10,105 @@
           </md-card-header>
           <md-card-content>
             <md-table
-              v-model="profile_patient"
+              v-model="profile_patient_list"
               :table-header-color="tableHeaderColor"
             >
-              <md-table-row slot="md-table-row" slot-scope="{ item }">
+              <md-table-row
+                slot="md-table-row"
+                slot-scope="{ item }"
+                @click="SetPatientInfo(item)"
+              >
                 <md-table-cell md-label="Họ và tên">{{
                   item.name
                 }}</md-table-cell>
               </md-table-row>
             </md-table>
-            <md-button class="md-round md-success">Thêm hồ sơ</md-button>
+            <!-- <md-button class="md-round md-success">Thêm hồ sơ</md-button> -->
+            <v-dialog
+              transition="dialog-top-transition"
+              max-width="600"
+              v-model="dialog"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  class="white--text"
+                  color="green lighten-1"
+                  v-bind="attrs"
+                  v-on="on"
+                  >Thêm hồ sơ</v-btn
+                >
+              </template>
+              <template>
+                <v-card>
+                  <v-toolbar color="green lighten-1" dark>Thêm Hồ Sơ</v-toolbar>
+                  <v-card-text>
+                    <v-form ref="form" v-model="valid" lazy-validation>
+                      <v-text-field
+                        v-model="new_patient_info.name"
+                        label="Họ và tên"
+                        required
+                      ></v-text-field>
+                      <v-menu
+                        v-model="menu2"
+                        :close-on-content-click="false"
+                        :nudge-right="40"
+                        transition="scale-transition"
+                        offset-y
+                        min-width="auto"
+                      >
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-text-field
+                            v-model="new_patient_info.birthday"
+                            label="Ngày sinh"
+                            prepend-icon="mdi-calendar"
+                            readonly
+                            v-bind="attrs"
+                            v-on="on"
+                          ></v-text-field>
+                        </template>
+                        <v-date-picker
+                          v-model="new_patient_info.birthday"
+                          @input="menu2 = false"
+                          locale="vi-cn"
+                        ></v-date-picker>
+                      </v-menu>
+
+                      <v-text-field
+                        v-model="new_patient_info.phone"
+                        label="Số điện thoại"
+                        required
+                      ></v-text-field>
+
+                      <v-text-field
+                        v-model="new_patient_info.sex"
+                        label="Giới tính"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="new_patient_info.id"
+                        label="CMND"
+                        required
+                      ></v-text-field>
+
+                      <v-text-field
+                        v-model="new_patient_info.job"
+                        label="Nghề nghiệp"
+                        required
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="new_patient_info.ethnic"
+                        label="Dân tộc"
+                        required
+                      ></v-text-field>
+                    </v-form>
+                  </v-card-text>
+                  <v-card-actions class="justify-end">
+                    <v-btn text @click="AddNewPatientProfile">Thêm</v-btn>
+                    <v-btn text @click="dialog = false">Đóng</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </template>
+            </v-dialog>
           </md-card-content>
         </md-card>
       </div>
@@ -29,17 +118,14 @@
             <h4 class="title-patient">THÔNG TIN</h4>
           </md-card-header>
           <md-card-content>
-            <md-table
-              v-model="patient_info"
-              :table-header-color="tableHeaderColor"
-            >
-              <md-table-row slot="md-table-row" slot-scope="{ item }">
-                <p>Ngày sinh: {{ item.birthday }}</p>
-                <p>Số điện thoại: {{ item.phone }}</p>
-                <p>Giới tính: {{ item.sex }}</p>
-                <p>CMND: {{ item.sex }}</p>
-                <p>Nghề nghiệp: {{ item.job }}</p>
-                <p>Dân tộc: {{ item.ethnic }}</p>
+            <md-table :table-header-color="tableHeaderColor">
+              <md-table-row v-model="patient_info">
+                <p>Ngày sinh: {{ patient_info.birthday }}</p>
+                <p>Số điện thoại: {{ patient_info.phone }}</p>
+                <p>Giới tính: {{ patient_info.sex }}</p>
+                <p>CMND: {{ patient_info.id }}</p>
+                <p>Nghề nghiệp: {{ patient_info.job }}</p>
+                <p>Dân tộc: {{ patient_info.ethnic }}</p>
               </md-table-row>
             </md-table>
             <md-button
@@ -52,7 +138,7 @@
       </div>
     </div>
     <div class="md-layout select-day" v-show="visible_selectdate">
-      <SelectDate></SelectDate>
+      <SelectDate ref="dateSelect"></SelectDate>
       <div class="btns-selectdate">
         <md-button class="md-round md-success back-button">Quay lại</md-button>
         <md-button
@@ -63,7 +149,7 @@
       </div>
     </div>
     <div class="md-layout select-opd" v-show="visible_selectopd">
-      <SelectOPD></SelectOPD>
+      <SelectOPD ref="OPDSelect"> </SelectOPD>
       <div class="btns-selectdate">
         <md-button class="md-round md-success back-button">Quay lại</md-button>
         <md-button
@@ -91,7 +177,7 @@
       <div class="md-layout-item md-size-100">
         <div class="md-layout-item verify-column md-size-25">
           <div>Chuyên khoa</div>
-          <div>Da Liễu</div>
+          <div>{{ this.opdSelection }}</div>
         </div>
         <div class="md-layout-item verify-column md-size-25">
           <div>Bác sĩ</div>
@@ -99,7 +185,7 @@
         </div>
         <div class="md-layout-item verify-column md-size-25">
           <div>Ngày khám</div>
-          <div>07/12/2021</div>
+          <div>{{ this.dateSelection }}</div>
           <div>8:00 - 9:00</div>
         </div>
         <div class="md-layout-item verify-column md-size-25">
@@ -110,15 +196,15 @@
       <div class="md-layout-item">Thông tin bệnh nhân</div>
       <div class="md-layout-item md-size-60">
         <div class="md-layout-item verify-column md-size-50">
-          <div>Họ và tên: {{ this.patient_info[0].name }}</div>
-          <div>Ngày sinh: {{ this.patient_info[0].birthday }}</div>
-          <div>Giới tính: {{ this.patient_info[0].sex }}</div>
-          <div>Số điện thoại: {{ this.patient_info[0].phone }}</div>
+          <div>Họ và tên: {{ this.patient_info.name }}</div>
+          <div>Ngày sinh: {{ this.patient_info.birthday }}</div>
+          <div>Giới tính: {{ this.patient_info.sex }}</div>
+          <div>Số điện thoại: {{ this.patient_info.phone }}</div>
         </div>
         <div class="md-layout-item verify-column md-size-50">
-          <div>CMND: {{ this.patient_info[0].id }}</div>
-          <div>Dân tộc: {{ this.patient_info[0].ethnic }}</div>
-          <div>Nghề nghiệp: {{ this.patient_info[0].job }}</div>
+          <div>CMND: {{ this.patient_info.id }}</div>
+          <div>Dân tộc: {{ this.patient_info.ethnic }}</div>
+          <div>Nghề nghiệp: {{ this.patient_info.job }}</div>
         </div>
       </div>
       <div class="btns-verify">
@@ -143,23 +229,16 @@ export default {
   },
   data() {
     return {
+      dialog: false,
+      menu2: false,
+
       visible_selectprofile: true,
       visible_selectdate: false,
       visible_selectopd: false,
       visible_selectdoctor: false,
       visible_verify: false,
-      profile_patient: [
-        {
-          name: "Nguyễn Duy Thanh",
-        },
-        {
-          name: "Nguyễn Ngọc Tân",
-        },
-        {
-          name: "Hà Ngọc Thọ",
-        },
-      ],
-      patient_info: [
+
+      profile_patient_list: [
         {
           name: "Nguyễn Duy Thanh",
           birthday: "03/03/2000",
@@ -169,9 +248,51 @@ export default {
           job: "Sinh viên",
           ethnic: "Kinh",
         },
+        {
+          name: "Hà Ngọc Thọ",
+          birthday: "03/06/1999",
+          phone: "096253021312",
+          sex: "Nam",
+          id: "077200009999",
+          job: "Nhân viên văn phòng",
+          ethnic: "Kinh",
+        },
+        {
+          name: "Nguyễn Thúy Liễu",
+          birthday: "03/03/2000",
+          phone: "0962530448",
+          sex: "Nữ",
+          id: "077200005348",
+          job: "Kỹ sư",
+          ethnic: "Kinh",
+        },
       ],
+
+      patient_info: {
+        name: "",
+        birthday: "",
+        phone: "",
+        sex: "",
+        id: "",
+        job: "",
+        ethnic: "",
+      },
+
+      new_patient_info: {
+        name: "",
+        birthday: "",
+        phone: "",
+        sex: "",
+        id: "",
+        job: "",
+        ethnic: "",
+      },
+
+      dateSelection: "",
+      opdSelection: "",
     };
   },
+
   methods: {
     selectProfileComplete() {
       this.visible_selectprofile = false;
@@ -180,14 +301,32 @@ export default {
     SelectDateComplete() {
       this.visible_selectdate = false;
       this.visible_selectopd = true;
+      this.dateSelection = this.$refs.dateSelect.date;
     },
     SelectOPDComplete() {
       this.visible_selectopd = false;
       this.visible_selectdoctor = true;
+      this.opdSelection = this.$refs.OPDSelect.selected;
     },
     SelectDoctorComplete() {
       this.visible_selectdoctor = false;
       this.visible_verify = true;
+    },
+
+    SetPatientInfo(patient_select) {
+      this.patient_info.name = patient_select.name;
+      this.patient_info.birthday = patient_select.birthday;
+      this.patient_info.phone = patient_select.phone;
+      this.patient_info.sex = patient_select.sex;
+      this.patient_info.id = patient_select.id;
+      this.patient_info.job = patient_select.job;
+      this.patient_info.ethnic = patient_select.ethnic;
+    },
+
+    AddNewPatientProfile() {
+      console.log(this.new_patient_info);
+      this.dialog = false;
+      this.new_patient_info = {};
     },
   },
 };
@@ -244,5 +383,8 @@ export default {
 .btns-verify {
   width: 100%;
   margin-top: 100px;
+}
+.continue-button {
+  float: right;
 }
 </style>
--- frontend/src/pages/Appointment/SelectDate.vue
+++ frontend/src/pages/Appointment/SelectDate.vue
@@ -1,99 +1,20 @@
 <template>
   <div class="content">
     <div class="md-layout select-date">
-      <!-- calendar -->
-      <div class="calendar" v-if="calendar">
-        <div class="calendar-row-month"></div>
-        <!-- head -->
-        <div class="calendar-row head">
-          <div
-            v-for="(item, index) in dateOfWeek"
-            :key="index"
-            class="row-item"
-          >
-            <strong v-text="item" />
-          </div>
-        </div>
-        <!-- body -->
-        <div
-          v-for="(row, row_index) in calendar"
-          :key="row_index"
-          class="calendar-row"
-        >
-          <div
-            v-for="(cell, cell_index) in row"
-            :key="cell_index"
-            class="row-item"
-            @click="changeBackgroundRow(cell_index)"
-          >
-            <span v-text="cell ? cell : ''" />
-          </div>
-        </div>
-      </div>
+      <v-date-picker
+        v-model="date"
+        full-width
+        class="mt-4"
+        locale="vi-cn"
+        color="green lighten-1"
+      ></v-date-picker>
     </div>
   </div>
 </template>
 
 <style lang="scss">
-.calendar {
-  margin: auto;
-  //padding: 0 1em;
-  .calendar-row {
-    display: flex;
-    &.head {
-      & > div {
-        height: 64px;
-        //color: #036666;
-        color: white;
-        //background-color: #99E2B4;
-        background-color: #4caf50;
-        opacity: 0.87;
-        border: thin solid #78c6a3;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-    }
-
-    .row-item {
-      flex-grow: 1;
-
-      position: relative;
-      border-top: none;
-      border: thin solid #78c6a3;
-
-      min-width: 100px;
-      width: calc(100% / 7);
-      height: 100px;
-      background: #ffffff;
-      display: inline-block;
-
-      & > span {
-        position: absolute;
-        color: #7184a3;
-        text-align: center;
-        font-size: 16px;
-        height: 27px;
-        width: 27px;
-        padding: 3px;
-      }
-    }
-
-    .row-item:hover {
-      & > span {
-        background-color: #f9c74f;
-        border-radius: 50%;
-      }
-      cursor: pointer;
-    }
-  }
-  border: thin solid #78c6a3;
-}
-.select-date{
+.select-date {
   padding: 50px;
-}
-.continue-button {
-  float: right;
 }
 </style>
 
@@ -101,74 +22,10 @@
 export default {
   data() {
     return {
-      dateOfWeek: [
-        "Chủ nhật",
-        "Thứ hai",
-        "Thứ ba",
-        "Thứ tư",
-        "Thứ năm",
-        "Thứ sáu",
-        "Thứ bảy",
-      ],
-      calendar: null,
+      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
     };
-  },
-  created() {
-    // sau khi khởi tạo component thì mình gọi hàm buildCalendar này để hiển thị lịch tháng 1 năm 2021
-    this.buildCalendar(3, 2022);
-  },
-  methods: {
-    // lấy ngày đầu và ngày cuối trong tháng
-    getRangeMonth(numbMonth, numbYear) {
-      numbMonth = Number(numbMonth) || new Date().getMonth() + 1;
-      numbYear = Number(numbYear) || new Date().getFullYear();
-      return {
-        firstDay: new Date(`${numbYear}-${numbMonth}-1`),
-        lastDay: new Date(
-          `${numbYear}-${numbMonth}-${new Date(
-            numbYear,
-            numbMonth,
-            0
-          ).getDate()}`
-        ),
-      };
-    },
-    buildCalendar(month, year) {
-      let arr = [[]];
-      // lấy ngày đầu và ngày cuối trong tháng
-      let { firstDay, lastDay } = this.getRangeMonth(month, year);
-      // đếm ngày
-      let countDay = 1;
-      // nếu countDay lớn hơn số ngày cuối trong tháng thì mình dừng vòng lặp
-      for (let i = 1; countDay <= lastDay.getDate(); i++) {
-        for (let j = 0; j < 7; j++) {
-          // first loop
-          if (i == 1) {
-            if (j < firstDay.getDay()) arr[arr.length - 1].push(0);
-            else {
-              arr[arr.length - 1].push(countDay);
-              countDay++;
-            }
-          } else {
-            if (countDay > lastDay.getDate()) arr[arr.length - 1].push(0);
-            else {
-              arr[arr.length - 1].push(countDay);
-              countDay++;
-            }
-          }
-        }
-        if (countDay > lastDay.getDate()) break;
-        arr.push([]);
-      }
-      this.calendar = arr;
-    },
-    changeBackgroundRow: function (cell_index) {
-      var buttons = document.getElementsByClassName("row-item");
-      console.log(buttons);
-      var span = buttons[cell_index].getElementsByTagName("span");
-      console.log("amount of span" + span.length);
-      //span[0].style.backgroundColor = 'blue';
-    },
   },
 };
 </script>
