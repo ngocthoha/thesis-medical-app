5469244cf22cc1abc3311fc3f5a306fdf07829ab
Thanh Nguyen <thanhbkcse@gmail.com>
2022-04-17 09:58:12 +0700

    Thay đổi dashboard
    Từ giờ sẽ làm trên core UI này

---
+++ frontend_final/src/store/modules/appointment.js
@@ -0,0 +1,92 @@
+import service from "@/store/services/appointment";
+
+const state = {
+  speciality_list: [],
+  doctors_list: [],
+  doctor_appointment_list: [],
+  user_appointment_list: []
+};
+
+const mutations = {
+  SET_SPECIALITY_LIST: (state, list) => {
+    state.speciality_list = list;
+  },
+
+  SET_DOCTOR_LIST: (state, list) => {
+    state.doctors_list = list;
+  },
+
+  SET_DOCTOR_APPOINTMENT_LIST: (state, list) => {
+    state.doctor_appointment_list = list;
+  },
+
+  SET_USER_APPOINTMENT_LIST: (state, list) => {
+    state.user_appointment_list = list;
+  }
+};
+
+const actions = {
+  speciality_list({ commit }, params) {
+    return service.speciality_list(params).then(({ data }) => {
+      if (data.code == 1) {
+        commit("SET_SPECIALITY_LIST", data.results);
+      } else {
+        console.log("error");
+      }
+    });
+  },
+
+  getDoctorList_byDateAndSpeciality({ commit }, params) {
+    return service
+      .getDoctorList_byDateAndSpeciality(params)
+      .then(({ data }) => {
+        if (data.code == 1) {
+          commit("SET_DOCTOR_LIST", data.results);
+        } else {
+          console.log("error");
+        }
+      });
+  },
+
+  createAppointment({ commit }, params) {
+    return service.createAppointment(params).then(({ data }) => {
+      console.log("add new appointment success");
+    });
+  },
+
+  getAppointment_byDate_Doctor({ commit }, params) {
+    return service.getAppointment_byDate_Doctor(params).then(({ data }) => {
+      if (data.code == 1) {
+        commit("SET_DOCTOR_APPOINTMENT_LIST", data.results);
+      } else {
+        console.log("error");
+      }
+    });
+  },
+
+  getAppointment_byProfileId({ commit }, params) {
+    return service.getAppointment_byProfileId(params).then(({ data }) => {
+      if (data.code == 1) {
+        commit("SET_USER_APPOINTMENT_LIST", data.results);
+      } else {
+        console.log("error");
+      }
+    });
+  }
+};
+
+const getters = {
+  speciality_list: speciality_list => state.speciality_list,
+  doctors_list: doctors_list => state.doctors_list,
+  doctor_appointment_list: doctor_appointment_list =>
+    state.doctor_appointment_list,
+  user_appointment_list: user_appointment_list => state.user_appointment_list
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+};
---
+++ frontend_final/src/store/modules/auth.js
@@ -0,0 +1,44 @@
+import service from "@/store/services/auth";
+
+const state = {
+  access_token: null,
+  types: []
+};
+
+const mutations = {
+  SET_TOKEN: (state, token) => {
+    state.access_token = token;
+  },
+  SET_TYPE: (state, types) => {
+    state.types = types;
+  }
+};
+
+const actions = {
+  login({ commit }, params) {
+    return service.login(params).then(({ data }) => {
+      if (data.access_token != null && data.access_token != "") {
+        commit("SET_TOKEN", data.access_token);
+        commit("SET_TYPE", data.type);
+        // get the decoded payload and header
+        //var decoded = jwt.decode(token, {complete: true});;
+        console.log("login success");
+      } else {
+        console.log("fail success");
+      }
+    });
+  }
+};
+
+const getters = {
+  access_token: access_token => state.access_token,
+  types: types => state.types
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+};
