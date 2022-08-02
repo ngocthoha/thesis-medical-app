7ad484dc3fe8939c6ce5bdbd5e7ce0118e465add
thanhbkcse <thanhbkcse@gmail.com>
2022-08-02 18:29:38 +0700

    Update Login.vue

--- frontend_final/src/views/auth/Login.vue
+++ frontend_final/src/views/auth/Login.vue
@@ -67,20 +67,71 @@
       </v-container>
     </div> -->
     <div
-      style="height: 100%; background: linear-gradient(268.58deg, #DCF5FF 2.14%, rgba(220, 245, 255, 0) 98.24%);"
+      style="height: 100%; background:linear-gradient(268.58deg, #DCF5FF 2.14%, #FFFFFF 98.24%);"
     >
       <v-col cols="12" class="fill-height">
         <v-row class="fill-height">
-          <v-col cols="6" class="d-flex align-center mb-6">
+          <v-col cols="6" class="my-auto">
             <div>
               <v-img
                 src="@/assets/img/auth/image1-login.png"
-                max-height="400px"
-                max-width="620px"
+                max-height="50%"
+                max-width="75%"
+                class="mx-auto rounded-xl"
               ></v-img>
             </div>
           </v-col>
-          <v-col cols="6"> </v-col>
+          <v-col cols="6" class="my-auto">
+            <v-text-field
+              label="Solo"
+              placeholder="Tên đăng nhập"
+              solo
+              height="60px"
+              style="width: 467px;"
+              :rules="[v => !!v || 'Tên đăng nhập là bắt buộc!']"
+              rounded
+              v-model="user.username"
+            ></v-text-field>
+            <v-text-field
+              label="Solo"
+              placeholder="Mật khẩu"
+              solo
+              height="60px"
+              width="467px"
+              style="width: 467px"
+              type="password"
+              :rules="[v => !!v || 'Mật khẩu là bắt buộc!']"
+              v-model="user.password"
+              rounded
+            ></v-text-field>
+            <v-row class="mt-8 ml-2" style="width: 467px">
+              <v-checkbox
+                label="Nhớ tài khoản"
+                v-model="value"
+                value="value"
+                class="mt-0 pt-0"
+              ></v-checkbox>
+              <v-spacer></v-spacer>
+              <a class="text-decoration-underline">Quên mật khẩu</a></v-row
+            >
+            <v-btn
+              height="60"
+              width="467"
+              class="white--text"
+              elevation="1"
+              color="#046792"
+              rounded
+              style="font-size: 16px"
+              @click="submit"
+              >Đăng Nhập</v-btn
+            >
+            <v-row class="mt-8 ml-2">
+              <div class="text-center">
+                Bạn chưa có tài khoản?
+                <a class="ml-4" @click="signup">Đăng ký</a>
+              </div>
+            </v-row>
+          </v-col>
         </v-row>
       </v-col>
     </div>
