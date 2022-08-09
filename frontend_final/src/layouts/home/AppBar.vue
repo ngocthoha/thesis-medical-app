873f8f0888eceedbaa8f3d8927a93849bf802aa9
Thanh Nguyen <thanhbkcse@gmail.com>
2022-08-09 07:37:32 +0700

    Update AppBar.vue

--- frontend_final/src/layouts/home/AppBar.vue
+++ frontend_final/src/layouts/home/AppBar.vue
@@ -1,51 +1,59 @@
 <template>
-  <v-app-bar
-    id="default-app-bar"
-    app
-    color="#DCF5FF"
-    height="70"
-    elevation="1"
-    scroll-target="#scrolling-techniques-7"
-  >
-    <!-- <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer" /> -->
-
-    <!-- <default-drawer-toggle class="hidden-sm-and-down" /> -->
-
-    <!-- <v-toolbar-title class="font-weight-light text-h5" v-text="name" /> -->
-    <v-spacer />
-    <v-col cols="5">
-      <v-row justify="center" no-gutters>
-        <v-btn
-          v-for="link in links"
-          :key="link.label"
-          color="#046792"
-          text
-          rounded
-          class="my-2"
-          @click="getpage(link)"
-        >
-          <strong style="font-size:14px">{{ link.label }}</strong>
-        </v-btn>
-      </v-row></v-col
+  <v-responsive aspect-ratio="18:1">
+    <v-app-bar
+      id="default-app-bar"
+      app
+      color="white"
+      elevation="1"
+      class="px-18 py-80"
+      scroll-target="#scrolling-techniques-7"
     >
+      <!-- <v-app-bar-nav-icon class="hidden-md-and-up" @click="drawer = !drawer" /> -->
 
-    <v-spacer />
+      <!-- <default-drawer-toggle class="hidden-sm-and-down" /> -->
 
-    <!-- <default-search class="hidden-sm-and-down" /> -->
+      <!-- <v-toolbar-title class="font-weight-light text-h5" v-text="name" />
+      <v-spacer />
+      <v-col cols="5">
+        <v-row justify="center" no-gutters>
+          <v-btn
+            v-for="link in links"
+            :key="link.label"
+            color="#046792"
+            text
+            rounded
+            class="my-2"
+            @click="getpage(link)"
+          >
+            <strong style="font-size:14px">{{ link.label }}</strong>
+          </v-btn>
+        </v-row></v-col
+      > -->
 
-    <!-- <default-go-home /> -->
+      <!-- <v-spacer /> -->
 
-    <!-- <default-notifications />
+      <!-- <default-search class="hidden-sm-and-down" /> -->
+
+      <!-- <default-go-home /> -->
+
+      <!-- <default-notifications />
 
     <default-account /> -->
 
-    <v-btn color="#046792" rounded class="my-2 white--text mr-1" @click="login">
-      Đăng nhập
-    </v-btn>
-    <v-btn color="#046792" rounded class="my-2 white--text" @click="signup">
-      Đăng ký
-    </v-btn>
-  </v-app-bar>
+      <!-- <v-btn
+        color="#046792"
+        rounded
+        class="my-2 white--text mr-1"
+        @click="login"
+      >
+        Đăng nhập
+      </v-btn>
+      <v-btn color="#046792" rounded class="my-2 white--text" @click="signup">
+        Đăng ký
+      </v-btn> -->
+      <div style="padding: "></div>
+    </v-app-bar>
+  </v-responsive>
 </template>
 
 <script>
@@ -86,29 +94,29 @@ export default {
   computed: {},
 
   data: () => ({
-    links: [
-      {
-        label: "Trang chủ",
-        name: "Trang chủ"
-      },
-      {
-        label: "Giới thiệu",
-        name: "Giới thiệu"
-      },
-      {
-        label: "Quy trình",
-        name: "Quy trình"
-      },
-      {
-        label: "Dịch vụ",
-        name: "Dịch vụ",
-        path: "/service"
-      },
-      {
-        label: "Liên hệ",
-        name: "Liên hệ"
-      }
-    ]
+    // links: [
+    //   {
+    //     label: "Trang chủ",
+    //     name: "Trang chủ"
+    //   },
+    //   {
+    //     label: "Giới thiệu",
+    //     name: "Giới thiệu"
+    //   },
+    //   {
+    //     label: "Quy trình",
+    //     name: "Quy trình"
+    //   },
+    //   {
+    //     label: "Dịch vụ",
+    //     name: "Dịch vụ",
+    //     path: "/service"
+    //   },
+    //   {
+    //     label: "Liên hệ",
+    //     name: "Liên hệ"
+    //   }
+    // ]
   }),
 
   methods: {
