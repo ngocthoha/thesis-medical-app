174a700ec7d70e5eff85837f0fa1daef1970674a
thanhbkcse <thanhbkcse@gmail.com>
2022-10-20 19:41:59 +0700

    Update medical_examination.vue

--- frontend_final/src/views/doctor/medical_examination.vue
+++ frontend_final/src/views/doctor/medical_examination.vue
@@ -6,11 +6,49 @@
       </p>
     </v-footer>
     <v-data-table
+      v-model="selected"
       :headers="headers"
-      :items="desserts"
-      :items-per-page="5"
+      :items="list_appointment"
+      :single-select="singleSelect"
+      item-key="id"
+      show-select
       class="elevation-1"
-    ></v-data-table>
+      checkbox-color="#3C5E7E"
+    >
+      <template v-slot:top>
+        <div class="pa-3">
+          <v-btn>Hủy</v-btn>
+        </div>
+        <v-switch
+          v-model="singleSelect"
+          label="Chọn một"
+          class="pa-3"
+          color="#537DA5"
+        ></v-switch>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+        <!-- <v-icon small class="mr-2" @click="editItem(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteItem(item)">
+          mdi-delete
+        </v-icon> -->
+
+        <v-btn color="#537DA5" elevation="0" class="btn"
+          ><p
+            class="ma-0 font-weight-medium"
+            style="color:white"
+            @click.stop="examinate(item)"
+          >
+            Khám
+          </p></v-btn
+        >
+      </template>
+    </v-data-table>
+
+    <v-dialog v-model="exam_dialog" width="600px">
+      <v-card>a</v-card>
+    </v-dialog>
   </v-card>
 </template>
 
@@ -18,102 +56,60 @@
 export default {
   data() {
     return {
+      singleSelect: false,
+      selected: [],
       headers: [
         {
-          text: "Dessert (100g serving)",
+          text: "Họ và tên đệm",
           align: "start",
           sortable: false,
-          value: "name"
+          value: "lastName"
         },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" }
+        { text: "Tên", value: "firstName" },
+        { text: "Ngày sinh", value: "dob" },
+        { text: "Giới tính", value: "gender" },
+        { text: "Ngày khám", value: "date_medical_examination" },
+        { text: "", value: "actions", sortable: false }
       ],
-      desserts: [
+      list_appointment: [
         {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: "1%"
+          id: "1",
+          firstName: "Thanh",
+          lastName: "Nguyễn Duy",
+          dob: "03/03/2000",
+          gender: "Nam",
+          date_medical_examination: "20/10/2022"
         },
         {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: "1%"
+          id: "2",
+          firstName: "Thọ",
+          lastName: "Hà Ngọc",
+          dob: "03/03/2003",
+          gender: "Nam",
+          date_medical_examination: "20/10/2022"
         },
         {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: "6%"
+          id: "3",
+          firstName: "Thúy",
+          lastName: "Nguyễn Thị",
+          dob: "03/03/2004",
+          gender: "Nữ",
+          date_medical_examination: "20/10/2022"
         }
-      ]
+      ],
+      exam_dialog: false
     };
+  },
+  methods: {
+    examinate(item) {
+      this.exam_dialog = true;
+    }
   }
 };
 </script>
+
+<style scoped>
+.btn {
+  text-transform: none;
+}
+</style>
