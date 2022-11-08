6a00e9572edc9142f870351fcc3fab4ab6b8eefd
Thanh Nguyen <thanhbkcse@gmail.com>
2022-11-09 00:53:28 +0700

    Update health_records.vue

--- frontend_final/src/views/user/health_records.vue
+++ frontend_final/src/views/user/health_records.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-card width="922px" elevation="4" class="pb-8">
-      <div>
+      <div class="d-flex flex-column">
         <!-- header -->
         <v-card height="132" class="px-8 d-flex flex-row pt-5" elevation="0">
           <div>
@@ -17,55 +17,122 @@
             class="mr-6 btn-not-transform text-body-2 white--text"
             elevation="0"
             color="#537DA5"
-            @click="close_view_appointment_detail"
+            @click="profile_list_dialog = true"
             >Chọn hồ sơ</v-btn
           >
         </v-card>
         <v-divider
           style="border-color: rgba(16, 24, 40, 0.03) !important"
         ></v-divider>
-        <!-- account -->
+        <!-- profile list dialog-->
+        <v-dialog v-model="profile_list_dialog" width="600">
+          <v-card class="pa-5">
+            <v-card
+              height="50"
+              class="d-flex align-center justify-center"
+              elevation="0"
+            >
+              <p style="color: #667085" class="text-body-1">
+                Vui lòng chọn hồ sơ
+              </p>
+            </v-card>
+            <v-divider
+              style="border-color: rgba(16, 24, 40, 0.03) !important"
+            ></v-divider>
+            <v-radio-group v-model="radioGroup" class="ma-0">
+              <v-card
+                v-for="n in 5"
+                :key="n"
+                class="pa-6 d-flex flex-row"
+                tile
+                elevation="0"
+              >
+                <v-avatar size="48" class="mr-5">
+                  <v-img src="@/assets/img/user/profile/avatar1.svg"></v-img>
+                </v-avatar>
+                <div class="d-flex flex-column justify-center">
+                  <p class="mb-2 font-weight-bold">Nguyễn Xuân Ngũ</p>
+                  <p class="ma-0 text-body-2" style="color: #667085">
+                    Chủ tài khoản
+                  </p>
+                </div>
+                <v-spacer></v-spacer>
+                <v-radio :value="n" color="#537DA5"></v-radio>
+              </v-card>
+            </v-radio-group>
+            <v-btn
+              class="mt-8  white--text text-body-1 btn-not-transform"
+              color="#537DA5"
+              elevation="0"
+              width="100%"
+              @click="view_health_record"
+              >Xác nhận
+            </v-btn>
+          </v-card>
+        </v-dialog>
+
+        <!-- profile -->
+        <v-card class="pa-6 d-flex flex-row" tile elevation="0" width="100%">
+          <v-avatar size="48" class="mr-5">
+            <v-img src="@/assets/img/user/profile/avatar1.svg"></v-img>
+          </v-avatar>
+          <div class="d-flex flex-column justify-center">
+            <p class="mb-2 font-weight-bold">Nguyễn Xuân Ngũ</p>
+            <p class="ma-0 text-body-2" style="color: #667085">
+              Chủ tài khoản
+            </p>
+          </div>
+        </v-card>
+        <v-divider
+          style="border-color: rgba(16, 24, 40, 0.03) !important"
+        ></v-divider>
+
         <!-- body -->
         <v-card class="pa-8" elevation="0">
-          <v-timeline>
-            <v-timeline-item
-              v-for="(year, i) in years"
-              :key="i"
-              :color="year.color"
-              small
-            >
-              <template v-slot:opposite>
-                <span
-                  :class="`headline font-weight-bold ${year.color}--text`"
-                  v-text="year.year"
-                ></span>
-              </template>
-              <v-card color="#EEF2F6">
-                <v-card-title style="color: #537DA5">
-                  11:00 - 12:00
-                </v-card-title>
-                <v-card-text class="white text--primary pa-3">
-                  <div class="d-flex flex-row">
-                    <p class="ma-0 mr-3 font-weight-bold">Cơ sở y tế:</p>
-                    <p class="ma-0">Bệnh viện đại học y dược</p>
-                  </div>
-                  <div class="d-flex flex-row">
-                    <p class="ma-0 mr-3 font-weight-bold">Dịch vụ:</p>
-                    <p class="ma-0">Khám theo nhu cầu</p>
-                  </div>
-                  <div class="d-flex flex-row mb-16">
-                    <p class="ma-0 mr-3 font-weight-bold">Bác sĩ:</p>
-                    <p class="ma-0">Nguyễn Xuân Đức</p>
-                  </div>
+          <v-scroll-y-transition>
+            <v-timeline v-show="years.length != 0">
+              <v-timeline-item
+                v-for="(year, i) in years"
+                :key="i"
+                :color="year.color"
+                small
+              >
+                <template v-slot:opposite>
+                  <span
+                    :class="`headline font-weight-bold ${year.color}--text`"
+                    v-text="year.year"
+                  ></span>
+                </template>
+                <v-card color="#EEF2F6">
+                  <v-card-title style="color: #537DA5">
+                    11:00 - 12:00
+                  </v-card-title>
+                  <v-card-text class="white text--primary pa-3">
+                    <div class="d-flex flex-row">
+                      <p class="ma-0 mr-3 font-weight-bold">Cơ sở y tế:</p>
+                      <p class="ma-0">Bệnh viện đại học y dược</p>
+                    </div>
+                    <div class="d-flex flex-row">
+                      <p class="ma-0 mr-3 font-weight-bold">Dịch vụ:</p>
+                      <p class="ma-0">Khám theo nhu cầu</p>
+                    </div>
+                    <div class="d-flex flex-row mb-8">
+                      <p class="ma-0 mr-3 font-weight-bold">Bác sĩ:</p>
+                      <p class="ma-0">Nguyễn Xuân Đức</p>
+                    </div>
 
-                  <v-btn :color="year.color" outlined class="btn-not-transform">
-                    Xem chi tiết
-                  </v-btn>
-                </v-card-text>
-              </v-card>
-            </v-timeline-item>
-          </v-timeline>
-          <v-dialog></v-dialog>
+                    <v-btn
+                      :color="year.color"
+                      outlined
+                      class="btn-not-transform"
+                    >
+                      Xem chi tiết
+                    </v-btn>
+                  </v-card-text>
+                </v-card>
+              </v-timeline-item>
+            </v-timeline>
+          </v-scroll-y-transition>
         </v-card>
       </div>
     </v-card>
@@ -74,29 +141,22 @@
 <script>
 export default {
   data: () => ({
-    years: [
-      {
-        color: "#537DA5",
-        year: "17/12/1960"
-      },
-      {
-        color: "#537DA5",
-        year: "17/12/1960"
-      },
-      {
-        color: "#537DA5",
-        year: "17/12/1960"
-      },
-      {
-        color: "#537DA5",
-        year: "17/12/1960"
-      },
-      {
-        color: "#537DA5",
-        year: "17/12/1960"
+    profile_list_dialog: false,
+    list_record_show: false,
+    years: []
+  }),
+
+  methods: {
+    view_health_record() {
+      this.profile_list_dialog = false;
+      for (let i = 0; i < 6; i++) {
+        this.years.push({
+          color: "#537DA5",
+          year: "17/12/1960"
+        });
       }
-    ]
-  })
+    }
+  }
 };
 </script>
 <style scoped>
