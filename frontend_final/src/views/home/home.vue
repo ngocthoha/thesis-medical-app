b5762b3e7e09614d0c8b98eb2b08d8f4c377b6e3
thanhbkcse <thanhbkcse@gmail.com>
2022-11-11 18:49:32 +0700

    Update home.vue

--- frontend_final/src/views/home/home.vue
+++ frontend_final/src/views/home/home.vue
@@ -222,49 +222,7 @@
             <v-img src="@/assets/img/home/section1-doctor.svg"></v-img>
           </v-card>
         </v-card>
-
-        <!-- <v-sheet
-          class="rounded-circle hidden-lg-and-down"
-          height="72px"
-          width="72px"
-          color="#BACBDC"
-          style="margin-top: 268px; margin-left: 123px;"
-        ></v-sheet>
-        <div class="d-flex align-center">
-          <v-card
-            color="#EEF2F6"
-            elevation="0"
-            class="d-flex align-end ml-16"
-            min-height="100"
-          >
-            <v-img
-              src="@/assets/img/home/Rectangle39.svg"
-              class="ml-16"
-              style="position: absolute"
-            ></v-img>
-            <v-img src="@/assets/img/home/toppng1.svg"> </v-img>
-          </v-card>
-        </div>
-
-        <v-sheet
-          class="rounded-circle hidden-md-and-down"
-          height="116px"
-          width="116px"
-          color="#BACBDC"
-          style="margin-top: 12px; margin-left: 56px;"
-        ></v-sheet> -->
       </v-card>
-
-      <!-- <v-card width="100%" min-height="500px" color="#EEF2F6">
-        <v-row>
-          <v-col md="6" sm="12" align-self="end">
-            <v-card width="50" height="50">a</v-card>
-          </v-col>
-          <v-col md="6" sm="12" align-self="start">
-            <v-card width="50" height="50">a</v-card>
-          </v-col>
-        </v-row>
-      </v-card> -->
 
       <!-- section 2 -->
       <v-layout>
@@ -284,46 +242,137 @@
             <v-card
               elevation="0"
               class="d-flex align-center mt-8"
-              width="720px"
+              width="60%"
               height="76px"
               outlined
             >
-              <v-card elevation="0"
-                ><p
-                  class="ma-0 font-weight-medium text-body-1 ml-3"
-                  style="color: #667085"
-                >
-                  Search Title
-                </p></v-card
-              >
-              <v-spacer />
-              <v-divider inset vertical></v-divider>
-              <v-menu>
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    color="#667085"
+              <v-menu bottom :offset-y="true" v-model="show_search_menu">
+                <template v-slot:activator="{}">
+                  <v-card
+                    class="d-flex flex-row align-center"
+                    width="100%"
                     elevation="0"
-                    class="font-weight-medium text-body-1 btn"
-                    text
-                    v-bind="attrs"
-                    v-on="on"
-                    width="135px"
-                    height="44px"
                   >
-                    Địa điểm <v-icon right>mdi-chevron-down</v-icon>
-                  </v-btn>
+                    <v-text-field
+                      full-width
+                      solo
+                      flat
+                      placeholder="Tìm kiếm"
+                      hide-details=""
+                    ></v-text-field>
+
+                    <v-divider class="mx-2" inset vertical></v-divider>
+                    <v-menu bottom :offset-y="true">
+                      <template v-slot:activator="{ attrs, on }">
+                        <v-btn
+                          color="#667085"
+                          elevation="0"
+                          class="font-weight-medium text-body-1 btn"
+                          text
+                          v-bind="attrs"
+                          v-on="on"
+                          width="135px"
+                          height="44px"
+                        >
+                          Địa điểm <v-icon right>mdi-chevron-down</v-icon>
+                        </v-btn>
+                      </template>
+                    </v-menu>
+                    <v-btn
+                      color="#537DA5"
+                      elevation="0"
+                      class="white--text btn font-weight-medium text-body-1 mr-3"
+                      @click="show_search_menu = true"
+                      >Tìm kiếm
+                      <v-img
+                        class="ml-3"
+                        src="@/assets/img/home/search_icon.svg"
+                      ></v-img
+                    ></v-btn>
+                  </v-card>
                 </template>
+                <!-- search result -->
+                <v-card class="d-flex flex-column pa-3" width="100%">
+                  <!-- hospital -->
+
+                  <v-card class="d-flex flex-column" elevation="0" width="100%">
+                    <p class="font-weight-bold">Cơ sở y tế</p>
+                    <!-- <p class="font-weight-bold">Cơ sở y tế</p> -->
+                    <div class="d-flex justify-center">
+                      <v-card width="1000" class="d-flex flex-wrap">
+                        <v-card
+                          v-for="n in 4"
+                          :key="n"
+                          width="500"
+                          height="150"
+                          class="d-flex flex-row pa-3 align-center"
+                          elevation="0"
+                        >
+                          <v-card>
+                            <v-img
+                              height="80"
+                              width="80"
+                              src="@/assets/img/home/hospital_avt.png"
+                            ></v-img>
+                          </v-card>
+                          <div class="ml-2">
+                            <p class="ma-0">
+                              Bệnh viện đại học y dược
+                            </p>
+                            <p class="ma-0">
+                              08 Đ. Nguyễn Hữu Cảnh, Phường 22, Bình Thạnh,
+                              Thành phố Hồ Chí Minh
+                            </p>
+                          </div>
+                        </v-card>
+                      </v-card>
+                    </div>
+                  </v-card>
+                  <!-- <v-card class="d-flex flex-row" elevation="0">
+                    <v-card
+                      class="d-flex flex-column"
+                      width="50%"
+                      elevation="0"
+                    >
+                      <p class="font-weight-bold">Bác sĩ</p>
+
+                      <v-card
+                        v-for="n in 4"
+                        :key="n"
+                        class="pa-3 mb-3"
+                        width="100%"
+                        elevation="0"
+                      >
+                        <v-img
+                          height="80"
+                          width="80"
+                          src="@/assets/img/home/hospital_avt.png"
+                        ></v-img>
+                      </v-card>
+                    </v-card>
+                    <v-card
+                      class="d-flex flex-column pr-8"
+                      width="50%"
+                      elevation="0"
+                    >
+                      <p class="font-weight-bold">Dịch vụ</p>
+                      <v-card
+                        v-for="n in 4"
+                        :key="n"
+                        class="pa-3 mb-3"
+                        width="100%"
+                        elevation="0"
+                      >
+                        <v-img
+                          height="80"
+                          width="80"
+                          src="@/assets/img/home/hospital_avt.png"
+                        ></v-img>
+                      </v-card>
+                    </v-card>
+                  </v-card> -->
+                </v-card>
               </v-menu>
-              <v-btn
-                color="#537DA5"
-                elevation="0"
-                class="white--text btn font-weight-medium text-body-1 mr-3"
-                >Tìm kiếm
-                <v-img
-                  class="ml-3"
-                  src="@/assets/img/home/search_icon.svg"
-                ></v-img
-              ></v-btn>
             </v-card>
           </v-card>
           <!---->
@@ -600,156 +649,164 @@
 <script>
 export default {
   data: () => ({
+    show_search_menu: false,
+    offset: true,
+    items: [
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me 2" }
+    ],
     fast_book_dialog: false,
     card_list_2: [
       {
         image: require("@/assets/img/home/home_icon1.png"),
         label: "Chẩn đoán hình ảnh",
-        name: "Chẩn đoán hình ảnh",
+        name: "Chẩn đoán hình ảnh"
       },
       {
         image: require("@/assets/img/home/home_icon2.png"),
         label: "Cơ - Xương - Khớp",
-        name: "Cơ - Xương - Khớp",
+        name: "Cơ - Xương - Khớp"
       },
       {
         image: require("@/assets/img/home/home_icon3.png"),
         label: "Nhi khoa",
-        name: "Nhi khoa",
+        name: "Nhi khoa"
       },
       {
         image: require("@/assets/img/home/home_icon4.png"),
         label: "Sản - Phụ Khoa",
-        name: "Sản - Phụ khoa",
+        name: "Sản - Phụ khoa"
       },
       {
         image: require("@/assets/img/home/home_icon5.png"),
         label: "Tai - Mũi - Họng",
-        name: "Tai - Mũi - Họng",
+        name: "Tai - Mũi - Họng"
       },
       {
         image: require("@/assets/img/home/home_icon6.png"),
         label: "Tim mạch",
-        name: "Tim mạch",
+        name: "Tim mạch"
       },
       {
         image: require("@/assets/img/home/home_icon7.png"),
         label: "Hô hấp",
-        name: "Hô hấp và miễn dịch dị ứng lâm sàng",
+        name: "Hô hấp và miễn dịch dị ứng lâm sàng"
       },
       {
         image: require("@/assets/img/home/home_icon8.png"),
         label: "Nội soi",
-        name: "Nội soi",
+        name: "Nội soi"
       },
       {
         image: require("@/assets/img/home/home_icon9.png"),
         label: "Tiêu hóa gan mật",
-        name: "Tiêu hóa gan mật",
+        name: "Tiêu hóa gan mật"
       },
       {
         image: require("@/assets/img/home/home_icon10.png"),
         label: "Xét nghiệm",
-        name: "Xét nghiệm",
+        name: "Xét nghiệm"
       },
       {
         image: require("@/assets/img/home/home_icon11.png"),
         label: "Khoa ngoại",
-        name: "Khoa ngoại",
+        name: "Khoa ngoại"
       },
       {
         image: require("@/assets/img/home/home_icon12.png"),
         label: "Khoa tâm thần",
-        name: "Tâm thần kinh",
-      },
+        name: "Tâm thần kinh"
+      }
     ],
 
     poular_search_list: [
       "Khám tai mũi họng",
       "Khám sản phụ khoa",
       "Nội soi tiêu hóa",
-      "Siêu âm 2D",
+      "Siêu âm 2D"
     ],
     service_list: [
       {
         img: require("@/assets/img/home/service1.svg"),
         title: "Gói Khám Gan nhiễm mỡ - Tổ hợp y tế MEDIPLUS",
         place: "Tổ Hợp Phòng Khám MEDIPLUS",
-        price: "2.480.000 đ",
+        price: "2.480.000 đ"
       },
       {
         img: require("@/assets/img/home/service2.svg"),
         title:
           "Gói khám tổng quát hệ tiêu hoá, gan mật tiêu chuẩn - Tổ hợp y tế MEDIPLUS",
         place: "Tổ Hợp Phòng Khám MEDIPLUS",
-        price: "2.220.000 đ",
+        price: "2.220.000 đ"
       },
       {
         img: require("@/assets/img/home/service3.svg"),
         title: "Gói Xét nghiệm tổng quát cho Nam - TTXN LABHOUSE",
         place: "Tổ Hợp Phòng Khám MEDIPLUS",
-        price: "1.335.000 đ",
+        price: "1.335.000 đ"
       },
       {
         img: require("@/assets/img/home/service4.svg"),
         title:
           "Test nhanh Cúm A,B cho người Nước ngoài tại Bệnh viện Đa khoa Bảo Sơn",
         place: "Tổ Hợp Phòng Khám MEDIPLUS",
-        price: "2.480.000 đ",
-      },
+        price: "2.480.000 đ"
+      }
     ],
     intro_list: [
       {
         img: require("@/assets/img/home/service_intro1.svg"),
         title: "Đặt khám từ xa",
         content:
-          "Bác sĩ của Blouse Care luôn sẵn sàng tư vấn và chăm sóc sức khỏe cho bạn mọi lúc mọi nơi qua video & chat.",
+          "Bác sĩ của Blouse Care luôn sẵn sàng tư vấn và chăm sóc sức khỏe cho bạn mọi lúc mọi nơi qua video & chat."
       },
       {
         img: require("@/assets/img/home/service_intro2.svg"),
         title: "Đội ngũ chuyên gia và cơ sở y tế hàng đầu",
         content:
-          "Dễ dàng kết nối với các bác sĩ ưu tú, tận tâm, có chuyên môn cao.",
+          "Dễ dàng kết nối với các bác sĩ ưu tú, tận tâm, có chuyên môn cao."
       },
       {
         img: require("@/assets/img/home/service_intro3.svg"),
         title: "Tiếp đón ưu tiên với nhiều quyền lợi đặc biệt",
         content:
-          "Đặt khám hẹn trước tại các bệnh viện trung ương, phòng khám hàng đầu.",
+          "Đặt khám hẹn trước tại các bệnh viện trung ương, phòng khám hàng đầu."
       },
       {
         img: require("@/assets/img/home/service_intro4.svg"),
         title: "Giá khám bằng với giá tại cơ sở y tế",
         content:
-          "Không những vậy còn giúp bạn tiết kiệm thời gian và các chi phí khác.",
-      },
+          "Không những vậy còn giúp bạn tiết kiệm thời gian và các chi phí khác."
+      }
     ],
     special_list: [
       {
         img: require("@/assets/img/home/ThanKinh.svg"),
-        title: "Thần kinh",
+        title: "Thần kinh"
       },
       {
         img: require("@/assets/img/home/Nhi.svg"),
-        title: "Nhi khoa",
+        title: "Nhi khoa"
       },
       {
         img: require("@/assets/img/home/CoXuongKhop.svg"),
-        title: "Cơ - Xương - Khớp",
+        title: "Cơ - Xương - Khớp"
       },
       {
         img: require("@/assets/img/home/San.svg"),
-        title: "Sản - Phụ khoa",
+        title: "Sản - Phụ khoa"
       },
       {
         img: require("@/assets/img/home/TaiMuiHong.svg"),
-        title: "Tai - Mũi - Họng",
+        title: "Tai - Mũi - Họng"
       },
       {
         img: require("@/assets/img/home/Tim.svg"),
-        title: "Tim mạch",
-      },
-    ],
+        title: "Tim mạch"
+      }
+    ]
   }),
 
   methods: {
@@ -764,7 +821,7 @@ export default {
     },
     getpage(name) {
       this.$router.push({ name: name });
-    },
+    }
   },
   computed: {
     columns() {
@@ -790,8 +847,8 @@ export default {
         return 24;
       }
       return 40;
-    },
-  },
+    }
+  }
 };
 </script>
 
