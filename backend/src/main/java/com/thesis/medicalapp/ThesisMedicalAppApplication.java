d9b7f4e64d0ac516c66e75ed9b85be7edd1706db
ngoctho <hangoctho2502@gmail.com>
2022-11-13 16:17:22 +0700

    update init

--- backend/src/main/java/com/thesis/medicalapp/ThesisMedicalAppApplication.java
+++ backend/src/main/java/com/thesis/medicalapp/ThesisMedicalAppApplication.java
@@ -3,10 +3,9 @@ package com.thesis.medicalapp;
 import com.thesis.medicalapp.config.ESConfig;
 import com.thesis.medicalapp.indices.Indices;
 import com.thesis.medicalapp.models.*;
+import com.thesis.medicalapp.models.HospitalService;
 import com.thesis.medicalapp.repository.*;
-import com.thesis.medicalapp.services.DoctorESService;
-import com.thesis.medicalapp.services.HospitalESService;
-import com.thesis.medicalapp.services.UserService;
+import com.thesis.medicalapp.services.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -59,7 +58,9 @@ public class ThesisMedicalAppApplication {
             HospitalHourRepository hospitalHourRepository,
             ESConfig esConfig,
             HospitalESService hospitalESService,
-            DoctorESService doctorESService
+            DoctorESService doctorESService,
+            ServiceRepository serviceRepository,
+            ServiceESService serviceESService
     ) {
         return args -> {
             // Remove all data in ES
@@ -74,7 +75,7 @@ public class ThesisMedicalAppApplication {
             userService.saveRole(new Role(null, "ROLE_USER"));
             userService.saveRole(new Role(null, "ROLE_ADMIN"));
             userService.saveRole(new Role(null, "ROLE_DOCTOR"));
-            Address address = new Address(null, "Viet Nam", "Tinh", "Huyen", "Xa", "");
+            Address address = new Address(null, "Việt Nam", "Hồ Chí Minh", "Quận 5", "Phường 11", "215 Hồng Bàng");
             Address address1 = addressRepository.save(address);
             String hospitalTime = "7:00 - 17:00";
             String hospitalBreak = "12:00 - 13:00";
@@ -83,7 +84,7 @@ public class ThesisMedicalAppApplication {
             Hospital hospital = new Hospital();
             hospital.setName("Bệnh Viện Đại Học Y Dược HCM");
             hospital.setAddress(address1);
-            hospital.setInfo("info");
+            hospital.setInfo("Bệnh viện có 3 cơ sở, trụ sở chính tại 215 Hồng Bàng, P.11, Q.5. Từ năm 2013, Bệnh viện đưa vào hoạt động tòa nhà 17 tầng đã mở rộng khả năng đáp ứng số lượng người bệnh đến khám ngoại trú và điều trị nội trú tại BV. Đây là tòa nhà thiết kế theo tiêu chuẩn bệnh viện quốc tế, đem đến môi trường khám chữa bệnh tiện nghi, xanh – sạch – đẹp, an ninh giúp người bệnh và gia đình cảm thấy thoải mái hơn khi vào Bệnh viện. Mỗi năm, Bệnh viện tiếp nhận trung bình hơn 2 triệu lượt người khám ngoại trú (khoảng 7.000 người khám/ngày), điều trị nội trú 55.000 người, phẫu thuật khoảng 30.000 trường hợp.");
             hospital.setFavorite(0.0);
             hospital.setRegistrationNumber(0);
             hospital.setIsActive(Boolean.TRUE);
@@ -92,6 +93,21 @@ public class ThesisMedicalAppApplication {
             hospital.setImageUrl("https://ump.edu.vn/img/image-default.png");
             Hospital hospital1 = hospitalRepository.save(hospital);
             hospitalESService.save(hospital1);
+            // init service
+            HospitalService service = new HospitalService();
+            service.setSpecialty(SpecialtyType.CHUAN_DOAN_HINH_ANH);
+            service.setType(ScheduleType.ONLINE);
+            service.setHospital(hospital1);
+            service.setNumOfServicePerHour(2);
+            service.setPrice("100.000");
+            service.setName("Khám cơ xương khớp");
+            service.setImageUrl("https://isofhcare-backup.s3-ap-southeast-1.amazonaws.com/images/20190927_121721_552975_tai-mui-hong-crop-1.max-1800x1800_e35067f6_29c0_47b2_bf9a_0149821990b6.jpg");
+            service.setRegistrationNumber(0);
+            service.setFavorite(0.0);
+            service.setInfo("Dịch vụ Khám Tai - Mũi - Họng theo yêu cầu của Bệnh viện Việt Đức chuyên điều trị các bệnh lý và triệu chứng");
+            HospitalService service1 = serviceRepository.save(service);
+            serviceESService.save(service1);
+            // init user
             String bio = "Là giảng viên của trường Đại học Y dược Thái Nguyên nhiều năm kinh nghiệm, tận tình, nhiệt huyết. Đi đầu trong lĩnh vực dịch vụ y tế tại nhà trong khu vực.";
             User user = new User(null, "user", "+84326185282", "1234", true, null, new ArrayList<>());
             User admin = new User(null, "admin", "+84326185283","1234", true, null, new ArrayList<>());
