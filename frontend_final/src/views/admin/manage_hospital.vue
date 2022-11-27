1cd0caf406e85654a703585f83986802422b76aa
thanhbkcse <thanhbkcse@gmail.com>
2022-11-27 08:43:46 +0700

    Update manage_hospital.vue

--- frontend_final/src/views/admin/manage_hospital.vue
+++ frontend_final/src/views/admin/manage_hospital.vue
@@ -20,10 +20,10 @@
         ></v-text-field>
       </v-card>
       <v-card class="d-flex flex-row" elevation="0">
-        <v-btn icon @click="add_dialog = true" class="mr-3">
+        <v-btn icon @click="open_add_dialog()" class="mr-3">
           <v-icon>mdi-plus</v-icon>
         </v-btn>
-        <v-btn icon @click="add_dialog = true" class="mr-3">
+        <v-btn icon @click="open_edit_dialog()" class="mr-3">
           <v-icon>mdi-pencil-outline</v-icon>
         </v-btn>
         <v-btn icon @click="add_dialog = true">
@@ -43,9 +43,63 @@
     </v-card>
     <!-- some dialog here -->
     <!-- add/edit dialog -->
-    <v-dialog width="800" v-model="add_dialog">
-      <v-card height="800">
-        <p>Thêm</p>
+    <v-dialog width="800" v-model="submit_dialog">
+      <v-card height="600" class="pa-5 d-flex flex-column">
+        <!-- header -->
+        <p v-if="is_add_dialog" class="font-weight-bold text-h6">Thêm</p>
+        <p v-else class="font-weight-bold text-h6">Chỉnh sửa</p>
+
+        <!-- list field item -->
+        <div class="d-flex flex-wrap justify-space-between">
+          <!-- field -->
+          <div class="d-flex flex-column mb-5">
+            <!-- label -->
+            <p class="mb-2">Tên Bệnh viện</p>
+            <v-card outlined width="350">
+              <v-text-field flat solo hide-details=""></v-text-field>
+            </v-card>
+          </div>
+          <!-- field -->
+          <div class="d-flex flex-column mb-3">
+            <!-- label -->
+            <p class="mb-2">Tên Bệnh viện</p>
+            <v-card outlined width="350">
+              <v-text-field flat solo hide-details=""></v-text-field>
+            </v-card>
+          </div>
+          <div class="d-flex flex-column mb-3">
+            <!-- label -->
+            <p class="mb-2">Tên Bệnh viện</p>
+            <v-card outlined width="350">
+              <v-text-field flat solo hide-details=""></v-text-field>
+            </v-card>
+          </div>
+        </div>
+
+        <v-spacer></v-spacer>
+        <!-- submit/cancle -->
+        <v-card width="100%" class="d-flex flex-row justify-space-between">
+          <v-btn
+            elevation="0"
+            outlined
+            color="#D0D5DD"
+            @click="submit_dialog = false"
+            class="btn"
+            ><p class="ma-0 font-weight-bold" style="color:#667085">
+              Hủy
+            </p></v-btn
+          >
+          <v-btn
+            v-if="is_add_dialog"
+            class="btn white--text"
+            color="#537DA5"
+            elevation="0"
+            >Thêm mới</v-btn
+          >
+          <v-btn v-else class="btn white--text" color="#537DA5" elevation="0"
+            >Lưu chỉnh sửa</v-btn
+          >
+        </v-card>
       </v-card>
     </v-dialog>
   </v-card>
@@ -56,7 +110,8 @@ export default {
   data() {
     return {
       //show dialog
-      add_dialog: false,
+      submit_dialog: false,
+      is_add_dialog: true,
       //table
       headers: [
         {
@@ -82,6 +137,23 @@ export default {
         }
       ]
     };
+  },
+  methods: {
+    open_add_dialog() {
+      this.is_add_dialog = true;
+      this.submit_dialog = true;
+    },
+
+    open_edit_dialog() {
+      this.is_add_dialog = false;
+      this.submit_dialog = true;
+    }
   }
 };
 </script>
+
+<style scoped>
+.btn {
+  text-transform: none;
+}
+</style>
